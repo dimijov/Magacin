2da09c4810a22962b707649cf1099061d3a44993
jyxop <2000dimitrije@gmail.com>
2023-04-06 14:27:21 +0200

    Implementirane metode interfejsa u klasi Magazin

--- Magacin/src/magacin/Magazin.java
+++ Magacin/src/magacin/Magazin.java
@@ -1,25 +1,33 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import artikal.Artikal;
 import interfejs.SpecifikacijaInterface;
 
 public class Magazin implements SpecifikacijaInterface {
 
+	List<Artikal> artikli=new ArrayList<>();
+	
 	@Override
 	public void dodajArtikal(Artikal a) {
-		
-		
+		artikli.add(a);
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal a) {
-		
+		artikli.remove(a);
 		
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		
+		for (Artikal artikal : artikli) {
+			if(artikal.getSifra()==sifra) {
+				return artikal;
+			}
+		}
 		return null;
 	}
 
