c7678116d783a303fc9646215432f38842ce6965
jyxop <2000dimitrije@gmail.com>
2023-04-06 14:20:01 +0200

    Napravljena klasa Magazin koja implementira interfejs

---
+++ Magacin/src/magacin/Magazin.java
@@ -0,0 +1,26 @@
+package magacin;
+
+import artikal.Artikal;
+import interfejs.SpecifikacijaInterface;
+
+public class Magazin implements SpecifikacijaInterface {
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a) {
+		
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		
+		return null;
+	}
+
+}
