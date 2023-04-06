298f658fb5cbcb2767237aa1ef7406e7fa6d6a44
jyxop <2000dimitrije@gmail.com>
2023-04-06 14:40:52 +0200

    Dodata logicka kontrola u klasi Magazin

--- Magacin/src/magacin/Magazin.java
+++ Magacin/src/magacin/Magazin.java
@@ -13,17 +13,27 @@ public class Magazin implements SpecifikacijaInterface {
 	
 	@Override
 	public void dodajArtikal(Artikal a) {
+		if(a==null) {
+			throw new NullPointerException("artikal ne sme biti null");
+		}
 		artikli.add(a);
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal a) {
+		if(a==null) {
+			throw new NullPointerException("artikal ne sme biti null");
+		}
 		artikli.remove(a);
 		
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
+		if(sifra<0) {
+			throw new IllegalArgumentException("sifra ne sme biti null");
+		}
+		
 		for (Artikal artikal : artikli) {
 			if(artikal.getSifra()==sifra) {
 				return artikal;
